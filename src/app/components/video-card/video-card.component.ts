00c047896c30c754bba68c32e9e26eec18d9328f
ZANX3Y <74610540+ZANX3Y@users.noreply.github.com>
2024-05-20 22:25:20 +0200

    Improve list thumbnails

--- src/app/components/video-card/video-card.component.ts
+++ src/app/components/video-card/video-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router'
+import { SkeletonModule } from 'primeng/skeleton'
 import ListVideo from '../../../../shared/ListVideo'
 
 @Component({
@@ -7,10 +8,16 @@ import ListVideo from '../../../../shared/ListVideo'
   standalone: true,
     imports: [
         RouterLink,
+        SkeletonModule,
     ],
   templateUrl: './video-card.component.html',
   styleUrl: './video-card.component.sass'
 })
 export class VideoCardComponent {
     @Input() video!: ListVideo
+    @Input() thumbnailSize = '768x432'
+
+    get thumbnail() {
+        return this.video.thumb.replace(/_\d+x\d+\.jpg$/, `_${this.thumbnailSize}.jpg`)
+    }
 }
